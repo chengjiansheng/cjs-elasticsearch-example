063c98c0f8b3d32872cf2f95dc109f7c939e29fc
Cheng <jiansheng80@126.com>
2019-07-09 22:42:54 +0800

    first commit

---
+++ src/main/java/com/cjs/example/CjsElasticsearchExampleApplication.java
@@ -0,0 +1,12 @@
+package com.cjs.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjsElasticsearchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjsElasticsearchExampleApplication.class, args);
+	}
+}
